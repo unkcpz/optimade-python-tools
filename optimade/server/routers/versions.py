9ed33a0cfcab83968c757698b8ab586f2e1e7518
Casper Welzel Andersen <casper.andersen@epfl.ch>
2021-06-09 20:57:56 +0200

    Remove responses from /versions

--- optimade/server/routers/versions.py
+++ optimade/server/routers/versions.py
@@ -2,7 +2,6 @@ from fastapi import Request, APIRouter
 from fastapi.responses import Response
 
 from optimade.server.routers.utils import BASE_URL_PREFIXES
-from optimade.server.schemas import ERROR_RESPONSES
 
 router = APIRouter(redirect_slashes=True)
 
@@ -15,7 +14,6 @@ class CsvResponse(Response):
     "/versions",
     tags=["Versions"],
     response_class=CsvResponse,
-    responses=ERROR_RESPONSES,
 )
 def get_versions(request: Request) -> CsvResponse:
     """Respond with the text/csv representation for the served versions."""
